920b579efd0052dc55e4ddb63135cd4c1bc30595
AlphaDream1 <philippe.ferraris@gmail.com>
2019-05-13 11:17:45 +0200

    first commit

---
+++ app/src/main/java/com/ferraris/firebasetest/MainActivity.java
@@ -0,0 +1,119 @@
+package com.ferraris.firebasetest;
+
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.widget.Button;
+
+import com.ferraris.firebasetest.auth.ProfileActivity;
+import com.ferraris.firebasetest.base.BaseActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getFragmentLayout() {
+        return R.layout.activity_main;
+    }
+
+    //FOR DATA
+    private static final int RC_SIGN_IN = 123;
+
+    //FOR DESIGN
+    @BindView(R.id.main_activity_coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.main_activity_button_login)
+    Button buttonLogin;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.updateUIWhenResuming();
+    }
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton() {
+        if (this.isCurrentUserLogged()) {
+            this.startProfileActivity();
+        } else {
+            this.startSignInActivity();
+        }
+    }
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),// EMAIL
+                                        new AuthUI.IdpConfig.GoogleBuilder().build(), // GOOGLE
+                                        new AuthUI.IdpConfig.FacebookBuilder().build()))// FACEBOOK
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void startProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+    // --------------------
+    // UI
+    // --------------------
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message) {
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateUIWhenResuming() {
+        this.buttonLogin.setText(this.isCurrentUserLogged() ? getString(R.string.button_login_text_logged) : getString(R.string.button_login_text_not_logged));
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+}
